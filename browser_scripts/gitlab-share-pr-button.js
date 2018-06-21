7c2ba02b2d94b58563427e9c80b66c64632d9f76
Johan Tell <johan@mintcore.se>
2018-06-21 10:30:00 +0200

    Add script for adding a share pr button to gitlab

---
+++ browser_scripts/gitlab-share-pr-button.js
@@ -0,0 +1,40 @@
+function copyPrRequest() {
+  let prTitle = document.querySelector("h2.title").textContent.trim();
+  let href = document.location.href;
+
+  let string = `**${prTitle}**\n${href}`;
+
+  console.log(string);
+  copyToClipboard(string);
+}
+
+function copyToClipboard(str) {
+  const el = document.createElement('textarea');
+  el.value = str;
+  el.setAttribute('readonly', '');
+  el.style.position = 'absolute';
+  el.style.left = '-9999px';
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+};
+
+function createCopyButton() {
+  if(!document.location.href.includes("merge_requests")) return;
+
+  let prTitle = document.querySelector("h2.title");
+
+  if(!prTitle) return;
+
+  let button = document.createElement("button");
+  button.classList.add("btn");
+  button.textContent = "Copy PR string";
+  button.addEventListener("click", () => {
+    copyPrRequest();
+  });
+
+  prTitle.parentNode.insertBefore(button, prTitle.nextSibling);
+}
+
+createCopyButton();
